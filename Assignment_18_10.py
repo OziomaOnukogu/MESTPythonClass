f104b0463fc42c7462219cdad917b3530d0e6ce8
OziomaOnukogu <116321022+OziomaOnukogu@users.noreply.github.com>
2022-10-23 21:12:35 +0000

    This is my  first assignments and I did it on own
    I'm not sure if this is what is required of me, but this is what I was able to come up with.

---
+++ Assignment_18_10.py
@@ -0,0 +1,11 @@
+# Take user input 
+# Take first user input{number 1}
+# Take second user input {number 2}
+# Add input {number 1} + {number 2}
+# Print the sum of both numbers
+
+num1 = input('ENTER NUMBER')
+num2 = input('ENTER NUMBER')
+sum = int(num1) + int(num2)
+
+print("The sum is: ", sum)
